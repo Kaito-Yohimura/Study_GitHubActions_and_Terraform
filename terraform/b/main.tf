344f1882c6fb242e39f3cda8b5bb9eb43378cf9f
吉村　海斗 <kaito_yoshimura@jmas.co.jp>
2023-03-30 12:25:17 +0900

    test

---
+++ terraform/b/main.tf
@@ -0,0 +1,33 @@
+# Provider
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# Terraform
+terraform {
+  # required_version = "xx"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      # version = "=4.57.0"
+    }
+  }
+
+    # tfstateの格納先
+  backend "s3" {
+    bucket = "yoshimura-study-b"
+    key    = "test.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+# Resource
+resource "aws_instance" "web" {
+  ami           = "ami-067871d950411e643"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "yoshimura-study"
+  }
+}
